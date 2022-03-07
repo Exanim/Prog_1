d0b360d45e866a3a52d8ca8a11f4f3a49e9e456d
Exanim <72408474+Exanim@users.noreply.github.com>
2022-03-07 21:08:20 +0100

    Add files via upload

---
+++ ch14_2.cpp
@@ -0,0 +1,34 @@
+#include"std_lib_facilities.h"
+
+class B2 {
+public:
+	virtual void pvf() = 0;
+};
+
+class D21 : public B2 {
+public:
+	void pvf() override { cout << s << endl; }
+	string s;
+};
+
+class D22 : public B2 {
+public:
+	void pvf() override { cout << i << endl; }
+	void f(B2& b2Ref) { b2Ref.pvf(); }
+	int i;
+};
+
+int main()
+{
+	D21 d21;
+	d21.s = "this is a string";
+	
+	D22 d22;
+	d22.i = 10;
+
+	d22.f(d21);
+	d22.f(d22);
+
+	return 0;
+}
+
